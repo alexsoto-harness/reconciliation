1f8ca9de6490e5e732e9b7ba5d119466e3445295
alexsoto-harness <>
2025-06-05 14:18:33 +0000

    Project init

---
+++ src/main/java/io/harness/Reconciliation.java
@@ -0,0 +1,11 @@
+package io.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reconciliation {
+    public static void main(String[] args) {
+        SpringApplication.run(Reconciliation.class, args);
+    }
+}
